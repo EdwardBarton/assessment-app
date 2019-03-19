ea2707fb1596bcecdb80b909466f5e55c22256d3
Edward Barton <edward.barton@icloud.com>
2019-03-19 13:15:52 -0400

    add team info to Card Headers w/ ability to update wins/losses

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { Box, Button, Card, Heading } from '@untappd/components'
+import {
+  Box,
+  Button,
+  Card,
+  Heading,
+  TextInput,
+  ListItem,
+  Flex,
+  FormLabel,
+} from '@untappd/components'
 
 const API = 'http://localhost:5000'
 
@@ -25,6 +34,56 @@ class App extends Component {
     this.fetchData()
   }
 
+  // Update database upon clicking "Update" button
+  async handleClick(teamID) {
+    const putApiRoute = `${API}/conferences/1/teams/${teamID}`
+    const teamToUpdate = this.state.teams.filter(t => t.id === teamID)[0]
+
+    // Update database
+    const response = await axios.put(putApiRoute, {
+      wins: teamToUpdate.wins,
+      losses: teamToUpdate.losses,
+    })
+
+    // Copy app state w/o mutating original
+    const stateCopy = Object.assign({}, this.state)
+    stateCopy.teams = stateCopy.teams.slice()
+    stateCopy.teams[teamID - 1] = Object.assign({}, stateCopy.teams[teamID - 1])
+
+    // Update app state w/ response data from successful PUT request
+    stateCopy.teams[teamID - 1] = response.data
+    this.setState(stateCopy)
+  }
+
+  // Update app state as Win/Loss input values change
+  handleChange(e, teamID) {
+    // Store new winning/losing value
+    const newValue = Number(e.target.value)
+
+    // Ensure new win/loss value is a number >= 0
+    if (newValue >= 0) {
+      // Copy app state w/o mutating original
+      const stateCopy = Object.assign({}, this.state)
+      stateCopy.teams = stateCopy.teams.slice()
+      stateCopy.teams[teamID - 1] = Object.assign(
+        {},
+        stateCopy.teams[teamID - 1],
+      )
+
+      // Update team's wins or losses
+      if (e.target.classList.contains('win-input')) {
+        stateCopy.teams[teamID - 1].wins = newValue
+      } else {
+        stateCopy.teams[teamID - 1].losses = newValue
+      }
+
+      // Update app state
+      this.setState(stateCopy)
+    } else {
+      return
+    }
+  }
+
   render() {
     const { conference } = this.state
     const { teams } = this.state
@@ -42,14 +101,41 @@ class App extends Component {
         {teams.map(team => (
           <Card key={team.id} mb={3}>
             <Card.Header>
-              <Heading>
-                {team.name} {team.mascot}
+              <Heading p={3}>
+                {team.name} {team.mascot}: {team.coach}
+                <Box p={3}>
+                  <FormLabel htmlFor="win-input">Wins</FormLabel>
+                  <TextInput
+                    defaultValue={team.wins}
+                    mb={2}
+                    min="0"
+                    className="win-input"
+                    type="number"
+                    onChange={e => this.handleChange(e, team.id)}
+                  />
+                  <FormLabel htmlFor="loss-input">Losses</FormLabel>
+                  <TextInput
+                    defaultValue={team.losses}
+                    mb={2}
+                    min="0"
+                    id="loss-input"
+                    type="number"
+                    onChange={e => this.handleChange(e, team.id)}
+                  />
+                  <Button
+                    size="small"
+                    color="blue"
+                    onClick={() => this.handleClick(team.id)}
+                  >
+                    Update
+                  </Button>
+                </Box>
               </Heading>
             </Card.Header>
             <Card.Content>
-              <li>Player's</li>
-              <li>go</li>
-              <li>here</li>
+              <ListItem>Player's</ListItem>
+              <ListItem>Go</ListItem>
+              <ListItem>Here</ListItem>
             </Card.Content>
           </Card>
         ))}
