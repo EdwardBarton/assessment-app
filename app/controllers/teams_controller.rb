d3da393add687908fff87dadf82a488c7ed6b9c5
Edward Barton <edward.barton@icloud.com>
2019-03-19 13:15:06 -0400

    add update action to teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,4 +5,18 @@ class TeamsController < ApplicationController
 
     render json: @teams
   end
+
+  # PUT /teams/:id
+  def update
+    @team = Team.find(params[:id])
+    @team.update(team_params)
+
+    render json: @team
+  end
+
+  private
+  # Only allow the updating of wins and losses
+  def team_params
+    params.require(:team).permit(:wins, :losses)
+  end
 end
