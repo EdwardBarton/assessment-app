49d66679d9568d0d69b167001f25577a47fde902
Edward Barton <edward.barton@icloud.com>
2019-03-20 10:06:21 -0400

    fetch players and toggle their visibility upon clicking card header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,10 +34,43 @@ class App extends Component {
     this.fetchData()
   }
 
+  // Fetch team players and toggle visibility when "Players" button is clicked in Card.Header
+  async fetchTeamPlayers(team) {
+    const { teams } = this.state
+
+    // Only fetch players on initial click
+    if (!team.players) {
+      const getPlayersApiRoute = `${API}/conferences/1/teams/${team.id}/players`
+      const response = await axios.get(getPlayersApiRoute, { mode: 'no-cors' })
+
+      // Update team with players response
+      const updatedTeam = {
+        ...teams[team.id - 1],
+        players: response.data,
+        showPlayers: true,
+      }
+
+      // Update app state teams array w/ updated team
+      const teamsCopy = [...teams]
+      teamsCopy[team.id - 1] = updatedTeam
+      this.setState({ teams: teamsCopy })
+    } else {
+      // Copy team from app state immutably and toggle players visibility
+      const teamToUpdate = { ...teams[team.id - 1] }
+      teamToUpdate.showPlayers = !teamToUpdate.showPlayers
+
+      // Update app state teams array w/ updated team immutably
+      const teamsCopy = [...teams]
+      teamsCopy[team.id - 1] = teamToUpdate
+      this.setState({ teams: teamsCopy })
+    }
+  }
+
   // Update database upon clicking "Update" button
   async handleClick(teamID) {
+    const { teams } = this.state
     const putApiRoute = `${API}/conferences/1/teams/${teamID}`
-    const teamToUpdate = this.state.teams.filter(t => t.id === teamID)[0]
+    const teamToUpdate = { ...teams[teamID - 1] }
 
     // Update database
     const response = await axios.put(putApiRoute, {
@@ -45,40 +78,32 @@ class App extends Component {
       losses: teamToUpdate.losses,
     })
 
-    // Copy app state w/o mutating original
-    const stateCopy = Object.assign({}, this.state)
-    stateCopy.teams = stateCopy.teams.slice()
-    stateCopy.teams[teamID - 1] = Object.assign({}, stateCopy.teams[teamID - 1])
-
-    // Update app state w/ response data from successful PUT request
-    stateCopy.teams[teamID - 1] = response.data
-    this.setState(stateCopy)
+    // Copy teams from app state immutably and update w/ response data from successful PUT request
+    const teamsCopy = [...teams]
+    teamsCopy[teamID - 1] = response.data
+    this.setState({ teams: teamsCopy })
   }
 
   // Update app state as Win/Loss input values change
   handleChange(e, teamID) {
+    const { teams } = this.state
+
     // Store new winning/losing value
     const newValue = Number(e.target.value)
 
     // Ensure new win/loss value is a number >= 0
     if (newValue >= 0) {
-      // Copy app state w/o mutating original
-      const stateCopy = Object.assign({}, this.state)
-      stateCopy.teams = stateCopy.teams.slice()
-      stateCopy.teams[teamID - 1] = Object.assign(
-        {},
-        stateCopy.teams[teamID - 1],
-      )
+      // Copy teams app state immutably and update wins/losses
+      const teamsCopy = [...teams]
 
-      // Update team's wins or losses
       if (e.target.classList.contains('win-input')) {
-        stateCopy.teams[teamID - 1].wins = newValue
+        teamsCopy[teamID - 1].wins = newValue
       } else {
-        stateCopy.teams[teamID - 1].losses = newValue
+        teamsCopy[teamID - 1].losses = newValue
       }
 
       // Update app state
-      this.setState(stateCopy)
+      this.setState({ teams: teamsCopy })
     } else {
       return
     }
@@ -103,6 +128,14 @@ class App extends Component {
             <Card.Header>
               <Heading p={3}>
                 {team.name} {team.mascot}: {team.coach}
+                <Button
+                  mx={2}
+                  size="small"
+                  color="blue"
+                  onClick={() => this.fetchTeamPlayers(team)}
+                >
+                  Players
+                </Button>
                 <Box p={3}>
                   <FormLabel htmlFor="win-input">Wins</FormLabel>
                   <TextInput
@@ -132,11 +165,13 @@ class App extends Component {
                 </Box>
               </Heading>
             </Card.Header>
-            <Card.Content>
-              <ListItem>Player's</ListItem>
-              <ListItem>Go</ListItem>
-              <ListItem>Here</ListItem>
-            </Card.Content>
+            {team.showPlayers ? (
+              <Card.Content>
+                <ListItem>Player's</ListItem>
+                <ListItem>Go</ListItem>
+                <ListItem>Here</ListItem>
+              </Card.Content>
+            ) : null}
           </Card>
         ))}
       </Box>
